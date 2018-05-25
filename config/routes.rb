809b50ac57833b1f901bf763f0c5e4d7d64beb4d
Tomomi Shiba <follwthedot@gmail.com>
2018-05-25 13:45:30 +0900

    adding doses

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   get 'cocktails/new'
   get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :doses, only: [ :new, :create ]
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
   end
-
-  root to:'cocktails#index'
+  resources :doses, only: [:destroy]
 end
